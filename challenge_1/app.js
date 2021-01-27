2a4f9fdd99071ae3181d52dfd6db327f6e4774fd
jasonschreib <jwschreiber1@gmail.com>
2021-01-27 15:25:35 -0800

    Created skeleton of MVC framework functions

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,38 @@
+//Model - program state - the truth of my program
+
+//create initial gameBoard - this will always be kept in case reset is clicked
+var gameBoardInitial = [
+  [0, 0, 0]
+  [0, 0, 0]
+  [0, 0, 0]
+];
+
+//create gameBoard for playing state
+var gameBoardPlaying = [
+  [0, 0, 0]
+  [0, 0, 0]
+  [0, 0, 0]
+];
+
+//create didWin function that returns a boolean, ends the game, and calls display message in the view
+
+//create didTie function that returns a boolean, ends the game, and calls display message in the view
 
 
+
+//View - presentation
+
+//function for displaying the gamepiece on gameboard based on the model
+
+//function for displaying which user's (X or O) turn it is
+
+//function for displaying message if a user has won or there is a tie - called from model
+
+
+
+//Controller - user input
+
+//create clickHandler for click on placing piece
+
+//create clickHandler for clicking reset button
+//resets the gameBoard in model to gameBoard initial
