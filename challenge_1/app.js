d63effc57a88c108d9ac20367148eec267cc8354
jasonschreib <jwschreiber1@gmail.com>
2021-01-27 18:36:26 -0800

    'New Game' functionality implemented

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,18 +1,30 @@
 //Model - program state - the truth of my program
+//create gameBoard variable
+var gameBoard;
 
-//create initial gameBoard - this will always be kept in case reset is clicked
-var gameBoardInitial = [
-  [0, 0, 0]
-  [0, 0, 0]
-  [0, 0, 0]
-];
+//create reset function - resets the gameBoard
+var reset = function() {
+  gameBoard = [
+    [0, 0, 0]
+    [0, 0, 0]
+    [0, 0, 0]
+  ];
+  //create a message that appears saying it's a new game
+  //TODO
+  console.log('NEW GAME');
+};
 
-//create gameBoard for playing state
-var gameBoardPlaying = [
-  [0, 0, 0]
-  [0, 0, 0]
-  [0, 0, 0]
-];
+//create function to keep track of whose turn it is
+var player = function() {
+  //create first user as X
+  var user = 1;
+  //if user has addedPiece, then switch user
+};
+
+//create add piece function - access the correct box in the tictactow table and place a piece according to who is the current user
+var addPiece = function() {
+
+};
 
 //create didWin function that returns a boolean, ends the game, and calls display message in the view
 
@@ -33,6 +45,23 @@ var gameBoardPlaying = [
 //Controller - user input
 
 //create clickHandler for click on placing piece
+var clickPieces = document.getElementsByTagName("td");
+//iterate over the clickPieces array and add eventListeners to each square
+for (var i = 0; i < clickPieces.length; i++) {
+  clickPiece[i].addEventListener("click", addPiece);
+}
+
+
 
 //create clickHandler for clicking reset button
 //resets the gameBoard in model to gameBoard initial
+//create variable to refer to reset button
+var resetButton = document.getElementsByTagName("button");
+//create the event listener - have to iterate over array for some reason even though there's one item
+for (var i = 0; i < resetButton.length; i++) {
+  resetButton[i].addEventListener("click", reset);
+}
+
+
+//call the reset function to start the game
+reset();
