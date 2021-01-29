2656cc2b685dbb692f6cf2425b09cf158be0fc23
jasonschreib <jwschreiber1@gmail.com>
2021-01-29 14:53:26 -0800

    Successful setup of express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+//require express
+const express = require('express');
+//initialize the express framework
+const app = express();
+
+const port = process.env.port || 4000;
 
 
+
+
+app.listen(port, () => console.log(`Server running on PORT ${port}`));
+
+//Middleware functions
