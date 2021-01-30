c8e59e3e9843b71a371916786389c7551c4ea7a1
jasonschreib <jwschreiber1@gmail.com>
2021-01-30 11:31:46 -0800

    Post requests are successful

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,22 @@
+//require jQuery
+// var $ = require('jQuery');
 
 
+//Model
+//if the submit button is clicked, then trigger the post request
+var post = function() {
+};
+
+
+
+
+///View
+
+
+
+
+//Controller
+
+
+//Create event listener for the submit button
+// document.getElementById('submitButton').addEventListener("click", post());
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -6,8 +6,29 @@ const app = express();
 const port = process.env.port || 4000;
 
 
+//Middleware functions
+
+//serve up the index.html page that lives in the client
+app.use(express.static('client'));
+
+
+//POST method route
+app.post('/convertToCSV', function(req, res) {
+  console.log('POST');
+  res.send("POST request to homepage");
+});
+
+app.get('/getTest', function(req, res) {
+  console.log('GET');
+  res.send("GET request to homepage");
+});
+
+
+//parseBody function that will take in the JSON data from req.body and send it back as a response that is in CSV style
+var parseBody = (req, res, next) => {
+
+  next();
+};
 
 
 app.listen(port, () => console.log(`Server running on PORT ${port}`));
-
-//Middleware functions
