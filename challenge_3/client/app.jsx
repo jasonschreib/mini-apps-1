5ec1b590be5c465c7b1e5a0e024cafb97eef2eb0
jasonschreib <jwschreiber1@gmail.com>
2021-02-15 12:54:00 -0800

    Created package.json / dependencies / backbone of server and client

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
+import React from "react";
+import ReactDom from "reactdom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //TODO: STATE
+  }
+
+  render() {
+    <div>
+      Hello World!
+    </div>
+  }
+
+  //create functional component for F1
 
 
+  ReactDom.render(< App/>, document.getElementById('app'));
+}
---
+++ challenge_3/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 2000;
+
+//serve up the index.html page that lives in the client
+app.use(express.static('client'));
+
+
+
+
+
+app.listen(port, () => {
+  console.log('App is listening on port ' + port);
+})
+
