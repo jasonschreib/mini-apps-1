df69b1d4ea17f3979b7f4bf0e0e0be22945463ce
jasonschreib <jwschreiber1@gmail.com>
2021-02-15 22:29:12 -0800

    clickHandler for checkout working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -4,10 +4,41 @@ class App extends React.Component {
     //TODO: STATE
   }
 
+  // handleClickOnCheckout = this.handleClickOnCheckout.bind(this);
+
+  //create function to handle click on 'checkout' button
+  handleClickOnCheckout(e) {
+    e.preventDefault();
+    console.log('Clicked on checkout!!');
+  }
+
+  //create function to handle click on 'next' button
+
+
+  //create function to handle click on 'purchase' button
+
+
+
   render() {
     return (
       <div>
-        Hello World!
+        <h1>Form 1</h1>
+        <h2>Please enter the following information: Name, email, and password</h2>
+        <form>
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" name="name"></input>
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" name="email"></input>
+          </div>
+          <div>
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" name="password"></input>
+          </div>
+          <input type="submit" value="Checkout" onClick={this.handleClickOnCheckout}></input>
+        </form>
       </div>
     )
   }
