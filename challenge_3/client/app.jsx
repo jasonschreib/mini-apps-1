41f30e15cc37a646a5d30c2acb5bdb49fc4471c0
jasonschreib <jwschreiber1@gmail.com>
2021-02-18 00:31:34 -0800

    Client side finished

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,9 +1,16 @@
 //class component for the entire app
+
+/*
+  Current page with state
+  1) create method for checkout button - starting the form
+  2) grab each field and make sure it's in state
+  3) separate different forms into different components
+  */
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
-      currentUser : {
+    this.state = {
+      currentUser: {
         name: '',
         email: '',
         password: '',
@@ -18,7 +25,7 @@ class App extends React.Component {
         cvv: '',
         billingzip: ''
       },
-      currentPage : 0
+      currentPage: 0
     }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleClickOnCheckout = this.handleClickOnCheckout.bind(this);
@@ -31,10 +38,10 @@ class App extends React.Component {
     var name = event.target.name;
     var value = event.target.value;
     console.log('VAL', value);
-    this.setState( ({
-      currentUser : {
+    this.setState(({
+      currentUser: {
         ...this.state.currentUser,
-        [name] : value
+        [name]: value
       }
     }), () => {
       console.log('STATE USER', this.state.currentUser);
@@ -45,15 +52,6 @@ class App extends React.Component {
   handleClickOnCheckout(e) {
     e.preventDefault();
     console.log('Clicked on checkout!!');
-    //send post request with data from form
-    // axios.post('/newUser', fd)
-    //   .then((response) => {
-    //     console.log('success!', response);
-    //     return response.json();
-    //   })
-    //   .then((results) => {
-    //     console.log('RESULTS', results);
-    //   });
     //set state of currentPage to 1
     this.setState({
       currentPage: 1
@@ -66,7 +64,7 @@ class App extends React.Component {
     console.log('Clicked on the next button');
     //increment the state by one
     this.setState((state) => ({
-      currentPage : state.currentPage + 1
+      currentPage: state.currentPage + 1
     }), () => {
       //console.log the state
       console.log('STATE', this.state);
@@ -76,34 +74,35 @@ class App extends React.Component {
   //create function to handle click on 'purchase' button
   handleClickOnPurchase(event) {
     console.log('Clicked on purchase button - return back to homepage');
-    //sent a post request to server with the state as data
+    // sent a post request to server with the state as data
+    // send post request with data from form
+    axios.post('/newUser', this.state.currentUser)
+      .then(() => {
+        console.log('success!');
+      });
+    // reset state of currentPage to 0
     this.setState({
       currentPage: 0
     });
   }
-  /*
-  Current page with state
-  1) create method for checkout button - starting the form
-  2) grab each field and make sure it's in state
-  3) separate different forms into different components
-  */
+
 
   render() {
     //determine which page to render based on state
     return (
-    <div>
-      Hey there!!
-      {/* if the state is 0, then render the original checkout page (no form data) and pass in handleClickOnCheckout */}
-      {this.state.currentPage === 0 ? <Homepage handleClickOnCheckout={this.handleClickOnCheckout} /> : null}
-      {/* if the state is 1, then render the f1 page and pass in handleClickOnNext */}
-      {this.state.currentPage === 1 ? <F1 currentUser={this.state.currentUser} handleClickOnNext={this.handleClickOnNext} handleInputChange={this.handleInputChange}/> : null}
-      {/* if the state is 2, then render the f2 page and pass in handleClickOnNext */}
-      {this.state.currentPage === 2 ? <F2 currentUser={this.state.currentUser} handleClickOnNext={this.handleClickOnNext} handleInputChange={this.handleInputChange}/> : null}
-      {/* if the state is 3, then render the f2 page and pass in handleClickOnNext */}
-      {this.state.currentPage === 3 ? <F3 currentUser={this.state.currentUser} handleClickOnNext={this.handleClickOnNext} handleInputChange={this.handleInputChange} /> : null}
-      {/* if the state is 4, then render the f3 page and pass in handleClickOnNext */}
-
-    </div>
+      <div>
+        Hey there!!
+        {/* if the state is 0, then render the original checkout page (no form data) and pass in handleClickOnCheckout */}
+        {this.state.currentPage === 0 ? <Homepage handleClickOnCheckout={this.handleClickOnCheckout} /> : null}
+        {/* if the state is 1, then render the f1 page and pass in handleClickOnNext */}
+        {this.state.currentPage === 1 ? <F1 currentUser={this.state.currentUser} handleClickOnNext={this.handleClickOnNext} handleInputChange={this.handleInputChange} /> : null}
+        {/* if the state is 2, then render the f2 page and pass in handleClickOnNext */}
+        {this.state.currentPage === 2 ? <F2 currentUser={this.state.currentUser} handleClickOnNext={this.handleClickOnNext} handleInputChange={this.handleInputChange} /> : null}
+        {/* if the state is 3, then render the f2 page and pass in handleClickOnNext */}
+        {this.state.currentPage === 3 ? <F3 currentUser={this.state.currentUser} handleClickOnNext={this.handleClickOnNext} handleInputChange={this.handleInputChange} /> : null}
+        {/* if the state is 4, then render the f3 page and pass in handleClickOnNext */}
+        {this.state.currentPage === 4 ? <Summary currentUser={this.state.currentUser} handleClickOnPurchase={this.handleClickOnPurchase} /> : null}
+      </div>
     )
   }
 
@@ -158,32 +157,32 @@ var F2 = (props) => {
       <h2>Now please enter the following fields: Shipping Address and Phone Number</h2>
       <form id="shippingForm">
         <div>
-        <h3>Shipping Address</h3>
+          <h3>Shipping Address</h3>
           <div>
-          <label htmlFor="line1">Address 1: </label>
+            <label htmlFor="line1">Address 1: </label>
             <input type="text" id="line1" name="line1" value={props.currentUser.line1} onChange={props.handleInputChange} required></input>
           </div>
           <div>
-          <label htmlFor="line2">Address 2: </label>
+            <label htmlFor="line2">Address 2: </label>
             <input type="text" id="line2" name="line2" value={props.currentUser.line2} onChange={props.handleInputChange}></input>
           </div>
           <div>
-          <label htmlFor="city">City: </label>
+            <label htmlFor="city">City: </label>
             <input type="text" id="city" name="city" value={props.currentUser.city} onChange={props.handleInputChange} required></input>
           </div>
           <div>
-          <label htmlFor="state">State: </label>
+            <label htmlFor="state">State: </label>
             <input type="text" id="state" name="state" value={props.currentUser.state} onChange={props.handleInputChange} required></input>
           </div>
           <div>
-          <label htmlFor="zip">Zip: </label>
+            <label htmlFor="zip">Zip: </label>
             <input type="text" id="zip" name="zip" value={props.currentUser.name} onChange={props.handleInputChange} required></input>
           </div>
         </div>
         <div>
-        <h3>Phone Number</h3>
-        <div>
-          <label htmlFor="phone">Phone Number: </label>
+          <h3>Phone Number</h3>
+          <div>
+            <label htmlFor="phone">Phone Number: </label>
             <input type="text" id="phone" name="phone" value={props.currentUser.phone} onChange={props.handleInputChange} required></input>
           </div>
         </div>
@@ -196,34 +195,63 @@ var F2 = (props) => {
 //create functional component for F3
 var F3 = (props) => {
   return (
-  <div>
-    <h1>Form 3</h1>
-    <form id="creditcardForm">
-      <h2>Finally, please enter your credit card information: </h2>
-      <div>
-        <label htmlFor="creditCard">Credit Card Number: </label>
-        <input type="text" id="creditCard" name="creditCard" value={props.currentUser.creditCard} onChange={props.handleInputChange} required></input>
-      </div>
-      <div>
-        <label htmlFor="expiration">Expiration Date: </label>
-        <input type="text" id="expiration" name="expiration" value={props.currentUser.expiration} onChange={props.handleInputChange} required></input>
-      </div>
-      <div>
-        <label htmlFor="cvv">CVV: </label>
-        <input type="text" id="cvv" name="cvv" value={props.currentUser.cvv} onChange={props.handleInputChange} required></input>
-      </div>
-      <div>
-        <label htmlFor="billingzip">Billing zip code: </label>
-        <input type="text" id="billingzip" name="billingzip" value={props.currentUser.billingzip} onChange={props.handleInputChange} required></input>
-      </div>
-    </form>
-    <input type="submit" value="Next" onClick={props.handleClickOnNext}></input>
-  </div>
+    <div>
+      <h1>Form 3</h1>
+      <form id="creditcardForm">
+        <h2>Finally, please enter your credit card information: </h2>
+        <div>
+          <label htmlFor="creditCard">Credit Card Number: </label>
+          <input type="text" id="creditCard" name="creditCard" value={props.currentUser.creditCard} onChange={props.handleInputChange} required></input>
+        </div>
+        <div>
+          <label htmlFor="expiration">Expiration Date: </label>
+          <input type="text" id="expiration" name="expiration" value={props.currentUser.expiration} onChange={props.handleInputChange} required></input>
+        </div>
+        <div>
+          <label htmlFor="cvv">CVV: </label>
+          <input type="text" id="cvv" name="cvv" value={props.currentUser.cvv} onChange={props.handleInputChange} required></input>
+        </div>
+        <div>
+          <label htmlFor="billingzip">Billing zip code: </label>
+          <input type="text" id="billingzip" name="billingzip" value={props.currentUser.billingzip} onChange={props.handleInputChange} required></input>
+        </div>
+      </form>
+      <input type="submit" value="Next" onClick={props.handleClickOnNext}></input>
+    </div>
   )
 }
 
 //create functional component for Summary page
-
+var Summary = (props) => {
+  //return all the user's information
+  return (
+    <div>
+      <div>
+        <h1>User Information Summary</h1>
+        <ul>
+          <li>Name: {props.currentUser.name}</li>
+          <li>Email: {props.currentUser.email}</li>
+          <li>Password: {props.currentUser.password}</li>
+          <li>Address 1: {props.currentUser.line1}</li>
+          <li>Address 2: {props.currentUser.line2}</li>
+          <li>City: {props.currentUser.city}</li>
+          <li>State: {props.currentUser.state}</li>
+          <li>Zip: {props.currentUser.zip}</li>
+          <li>Phone Number: {props.currentUser.phone}</li>
+          <li>Credit Card: {props.currentUser.creditCard}</li>
+          <li>Expiration Date: {props.currentUser.expiration}</li>
+          <li>CVV: {props.currentUser.cvv}</li>
+          <li>Billing Zip: {props.currentUser.billingzip}</li>
+        </ul>
+      </div>
+      <div>
+        <form>
+          <input type="submit" value="Purchase" onClick={props.handleClickOnPurchase}></input>
+        </form>
+      </div>
+    </div>
+  )
+}
 
 
 
