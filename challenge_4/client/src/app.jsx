09e649a79daf3bc454c278a863100a8a6cea8ee2
jasonschreib <jwschreiber1@gmail.com>
2021-03-02 13:53:09 -0600

    Rendering colored circles on click of column

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 2]
+        [0, 0, 0, 0, 0, 0, 2]
       ],
       currentPlayer: 1
     }
@@ -27,38 +27,61 @@ class App extends React.Component {
     //change the state to reflect the click on the column
     //check the column that was passed in
     //if the element at row5, col is a zero
-    // if (this.state.board[5].row5[col] === 0) {
-    //   console.log('IN HERE FIRST');
-    //   //then change it to a 1 or 2 depending on which player's turn it is
-    //   this.setState({
-    //     board[5].row5[col] : 5;
-    //   });
-    //   //otherwise if the element at ro4, col is a zero
-    // } else if (this.state.board[4].row4[col] === 0) {
-    //   console.log('IN HERE');
-    //   //then change it to a 1 or 2 depending on which player's turn it is
+    if (this.state.board[5][col] === 0) {
+      console.log('IN HERE FIRST');
+      const newState = this.state.board.slice();
+      //then change it to a 1 or 2 depending on which player's turn it is
+      newState[5][col] = this.state.currentPlayer;
+      this.setState({
+        board : newState
+      });
+      //otherwise if the element at ro4, col is a zero
+    } else if (this.state.board[4][col] === 0) {
+      console.log('IN HERE');
+      const newState = this.state.board.slice();
+      //then change it to a 1 or 2 depending on which player's turn it is
+      newState[4][col] = this.state.currentPlayer;
+      this.setState({
+        board : newState
+      });
+      //otherwise if the element at row3, col is a zero
+    } else if (this.state.board[3][col] === 0) {
+      const newState = this.state.board.slice();
+      //then change it to a 1 or 2 depending on which player's turn it is
+      newState[3][col] = this.state.currentPlayer;
+      this.setState({
+        board : newState
+      });
+      //otherwise if the element at row2, col is a zero
+    } else if (this.state.board[2][col] === 0) {
+      const newState = this.state.board.slice();
+      //then change it to a 1 or 2 depending on which player's turn it is
+      newState[2][col] = this.state.currentPlayer;
+      this.setState({
+        board : newState
+      });
 
-    //   //otherwise if the element at row3, col is a zero
-    // } else if (this.state.board[3].row3[col] === 0) {
-    //   //then change it to a 1 or 2 depending on which player's turn it is
-    //   this.state.board[3].row3[col] = this.state.currentPlayer;
-    //   //otherwise if the element at row2, col is a zero
-    // } else if (this.state.board[2].row2[col] === 0) {
-    //   //then change it to a 1 or 2 depending on which player's turn it is
-    //   this.state.board[2].row2[col] = this.state.currentPlayer;
-    //   //otherwise if the element at row1, col is a zero
-    // } else if (this.state.board[1].row1[col] === 0) {
-    //   //then change it to a 1 or 2 depending on which player's turn it is
-    //   this.state.board[1].row1[col] = this.state.currentPlayer;
-    //   //otherwise if the element at row0, col is a zero
-    // } else if (this.state.board[0].row0[col] === 0) {
-    //   //then change it to a 1 or 2 depending on which player's turn it is
-    //   this.state.board[0].row0[col] = this.state.currentPlayer;
-    //   //otherwise, alert the user a piece cannot be added there
-    // } else {
-    //   //TODO: alert the user
-    //   console.log('Piece cannot be added there');
-    // }
+      //otherwise if the element at row1, col is a zero
+    } else if (this.state.board[1][col] === 0) {
+      const newState = this.state.board.slice();
+      //then change it to a 1 or 2 depending on which player's turn it is
+      newState[1][col] = this.state.currentPlayer;
+      this.setState({
+        board : newState
+      });
+      //otherwise if the element at row0, col is a zero
+    } else if (this.state.board[0][col] === 0) {
+      const newState = this.state.board.slice();
+      //then change it to a 1 or 2 depending on which player's turn it is
+      newState[0][col] = this.state.currentPlayer;
+      this.setState({
+        board : newState
+      });
+      //otherwise, alert the user a piece cannot be added there
+    } else {
+      //TODO: alert the user
+      console.log('Piece cannot be added there');
+    }
 
     console.log('BOARD', this.state.board);
     //test for a tie - if testForTie function is true
